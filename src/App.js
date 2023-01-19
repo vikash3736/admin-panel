98db08365821fcc23585fc0009f4abbfefbbfc0d
Vikash Kumar Mahto <emails2vikash@gmail.com>
2023-01-19 20:45:41 +0530

    Sidebar Component Work Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import './App.css'
 
 function App() {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">other page</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import './Sidebar.css'
+import LineStyleIcon from '@mui/icons-material/LineStyle';
+import TimelineIcon from '@mui/icons-material/Timeline';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import ReportIcon from '@mui/icons-material/Report';
+import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
+import BarChartIcon from '@mui/icons-material/BarChart';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem active">
+              <LineStyleIcon className='sidebarIcon' /> Home
+            </li>
+            <li className="sidebarListItem">
+              <TimelineIcon className='sidebarIcon' /> Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUpIcon className='sidebarIcon' /> Sales
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <PermIdentityIcon className='sidebarIcon' /> Users
+            </li>
+            <li className="sidebarListItem">
+              <StorefrontIcon className='sidebarIcon' /> Products
+            </li>
+            <li className="sidebarListItem">
+              <CurrencyRupeeIcon className='sidebarIcon' /> Transactions
+            </li>
+            <li className="sidebarListItem">
+              <BarChartIcon className='sidebarIcon' /> Reports
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <MailOutlineIcon className='sidebarIcon' /> Mail
+            </li>
+            <li className="sidebarListItem">
+              <DynamicFeedIcon className='sidebarIcon' /> Feedback
+            </li>
+            <li className="sidebarListItem">
+              <ChatBubbleOutlineIcon className='sidebarIcon' /> Messages
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <WorkOutlineIcon className='sidebarIcon' /> Manage
+            </li>
+            <li className="sidebarListItem">
+              <TimelineIcon className='sidebarIcon' /> Analytics
+            </li>
+            <li className="sidebarListItem">
+              <ReportIcon className='sidebarIcon' /> Reports
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
